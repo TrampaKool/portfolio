a1cb9bbbf57fd781a2b4245719423480159f228e
TrampaKool <labismail25@gmail.com>
2024-11-15 17:47:58 +0200

    Added projects + other

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
-const { useState, useEffect } = React;
 
 const allProjects = [
-    {name: "Random Quote Generator", link: "https://trampakool.github.io/random-quote-generator/", image: ""},
-    {name: "Drum Machine", link: "https://trampakool.github.io/drum-machine/", image: ""},
+    {name: "Random Quote Generator", link: "https://trampakool.github.io/random-quote-generator/", image: "./random-quote-generator.png"},
+    {name: "Drum Machine", link: "https://trampakool.github.io/drum-machine/", image: "./drum-machine.png"},
+    {name: "Calculator", link: "https://trampakool.github.io/calculator/", image: "./calculator.png"},
+    {name: "Markdown Previewer", link: "https://trampakool.github.io/markdown-previewer/", image: "./markdown-previewer.png"},
+    {name: "Pokemon Search App", link: "https://trampakool.github.io/pokemon-search-app/", image: "./pokemon-search-app.png"},
+    {name: "Decimal to Roman Numeral Converter", link: "https://trampakool.github.io/dec-to-roman/", image: "./dec-to-roman.png"},
 ];
 
-const API_KEY = "AIzaSyC-q-tKcK4pi-Nw5PwjxaGQmTB7wX7hDfc";
-
 const Projects = () => {
-    const [screenshots, setScreenshots] = useState({});
-
-    const getScreenShot = async (pageLink) => {
-        try {
-            const response = await fetch(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(pageLink)}&key=${API_KEY}&strategy=desktop
-            `);
-            const data = await response.json();
-
-            const screenshot = data.lighthouseResult.audits['final-screenshot'].details.data;
-            return screenshot;
-        } catch (error) {
-            console.error("Error while fetching data!", error);
-        }
-    }
-
-    useEffect(() => {
-        const fetchScreenshots = async () => {
-            const allScreenshots = {};
-            for (const project of allProjects) {
-                allScreenshots[project.link] = await getScreenShot(project.link);
-            }
-            setScreenshots(allScreenshots);
-        }
-
-        fetchScreenshots();
-    }, []);
 
     return (
         <>
@@ -44,7 +19,7 @@ const Projects = () => {
                         <p className="project-title">
                             <span className="text-deco">&lt;</span>
                             {el.name}
-                            <span className="text-deco">&gt;</span>
+                            <span className="text-deco">/&gt;</span>
                         </p>
                     </a>
                 ))
@@ -52,4 +27,5 @@ const Projects = () => {
         </>
     );
 };
+
 ReactDOM.render(<Projects />, document.getElementById("projects-grid"));
